5c14cfde17d82655c7f31f3d0e3c2b65e6f5bb20
lucas <lucas@lucasjsmith.com>
2015-07-27 19:18:59 -0700

    initial commit of existing page

---
+++ europe/geodata/iconLookup.js
@@ -0,0 +1,18 @@
+var iconLookupTable = [
+	{
+		'airport': 'icon: \'plane\', prefix: \'fa\', markerColor: \'cadetblue\'',
+		'station': 'icon: \'bus\', prefix: \'fa\', markerColor: \'cadetblue\'',
+		'hotel': 'icon: \'building\', prefix: \'fa\', markerColor: \'cadetblue\'',
+		'food': 'icon: \'cutlery\', prefix: \'fa\', markerColor: \'orange\'',
+		'church': 'icon: \'circle\', prefix: \'fa\', markerColor: \'blue\'',
+		'poi': 'icon: \'circle\', prefix: \'fa\', markerColor: \'blue\'',
+		'gelato': 'icon: \'icecream\', prefix: \'ion\', markerColor: \'orange\'',
+		'cafe': 'icon: \'coffee\', prefix: \'fa\', markerColor: \'orange\'',
+		'museum': 'icon: \'university\', prefix: \'fa\', markerColor: \'blue\'',
+		'boat': 'icon: \'anchor\', prefix: \'fa\', markerColor: \'cadetblue\'',
+		'mundial': 'icon: \'soccer-ball-o\', prefix: \'fa\', markerColor: \'green\'',
+		'bus': 'icon: \'bus\', prefix: \'fa\', markerColor: \'cadetblue\'',
+		'bar': 'icon: \'beer\', prefix: \'ion\', markerColor: \'orange\'',
+		'bike': 'icon: \'bike\', prefix: \'fa\', markerColor: \'cadetblue\''
+	}
+]
